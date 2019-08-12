90363848aebfe57a9550d70b137eef4a69dc0e06
Ashmeet Singh <ashmeet.singh@goibibo.com>
2019-08-12 19:53:16 +0530

    stapi urls

---
+++ constants/api_path.go
@@ -0,0 +1,16 @@
+package constants
+
+var (
+
+	// API_TAG specifies the type of api to be used for execution
+	API_TAG = "stapi"
+
+	// StURL exposes stapi url via constants
+	StURL = "http://stapi.co/api/v1/rest"
+
+	// REST path for character full search
+	CHARACTER_SUB_URL = "/character/search"
+
+	// REST path for character species search
+	SPECIES_SUB_URL = "/character"
+)
