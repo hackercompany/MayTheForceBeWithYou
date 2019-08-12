09086aa4f76c5a5c4a1b2bf4a34fcb7297b11870
Ashmeet Singh <ashmeet.singh@goibibo.com>
2019-08-12 19:47:08 +0530

    Error constants

---
+++ constants/error_names.go
@@ -0,0 +1,11 @@
+package constants
+
+// Errors in entire project are referenced from these variables
+var (
+	INVALID_INPUT        = "Input not valid"
+	TOO_GENERIC_NAME     = "Too many results for name"
+	SPECIES_NOT_FOUND    = "No species for this character found"
+	CHARACTER_NOT_FOUND  = "No character found"
+	API_STATUS_INVALID   = "Api Call Failed"
+	DICTIONARY_CORRUPTED = "Dictionary json corrupted"
+)
---
+++ lib/http_handler/handler.go
@@ -0,0 +1,51 @@
+package http_handler
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/hackercompany/MayTheForceBeWithYou/constants"
+	"github.com/hackercompany/MayTheForceBeWithYou/logger"
+)
+
+// Global http client to handle add requests
+var client = http.Client{
+	Timeout: time.Duration(10 * time.Second),
+}
+
+// RestCall takes care of all http calls for stapi library
+func CallRest(url, method, payload string) ([]byte, error) {
+
+	body := strings.NewReader(payload)
+
+	req, _ := http.NewRequest(method, url, body)
+
+	resp, err := client.Do(req)
+
+	// Handling network errors
+	if err != nil {
+
+		logger.Print("CallRest", err.Error())
+		return nil, err
+
+	}
+
+	// Body is closed to avoid memory leak
+	defer resp.Body.Close()
+
+	// Anything other than 200 will raise error
+	if resp.StatusCode != 200 {
+
+		logger.Print("CallRest", constants.API_STATUS_INVALID)
+		return nil, errors.New(constants.API_STATUS_INVALID)
+
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+
+	return content, err
+
+}
